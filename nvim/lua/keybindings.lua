98e612531d19a6a6cf7bb37aa51fa76453585d94
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-18 20:03:31 +0530

    Update neovim keybind configuration

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -12,35 +12,36 @@ local map = function(key)
 	opts.buffer = nil
   
 	if buffer then
-		vim.api.nvim_buf_set_keymap(0, key[1], key[2], key[3], opts)
+		vim.api.nvim_buf_set_keymap('n', 0, key[1], key[2], opts)
 	else
-		vim.api.nvim_set_keymap(key[1], key[2], key[3], opts)
+		vim.api.nvim_set_keymap('n', key[1], key[2], opts)
 	end
 end
 
 -- Window mapping
-map {'n', '<Leader>w', ':write<CR>'}
-map {'n', '<C-h>', '<C-w>h'}
-map {'n', '<C-j>', '<C-w>j'}
-map {'n', '<C-k>', '<C-w>k'}
-map {'n', '<C-l>', '<C-w>l'}
-map {'n', '<C-q>', ':wqa!<CR>'}
+map {'<Leader>w', ':write<CR>'}
+map {'<C-h>', '<C-w>h'}
+map {'<C-S-h>', '<C-w>H'}
+map {'<C-j>', '<C-w>j'}
+map {'<C-k>', '<C-w>k'}
+map {'<C-l>', '<C-w>l'}
+map {'<C-q>', ':wqa<CR>'}
 
 -- NERDTree Explorer
-map {'n', '<C-b>', ':lua require"tree".toggle()<CR>'}
+map {'<C-b>', ':lua require"tree".toggle()<CR>'}
 
 -- Saving
-map {'n', '<C-s>', ':wa!<CR>'}
+map {'<C-s>', ':wa<CR>'}
 
 -- Babar
-map {'n', '<Tab>', ':w!<CR>:BufferNext<CR>'}
-map {'n', '<S-Tab>', ':w!<CR>:BufferPrevious<CR>'}
-map {'n', '<C-w>', ':BufferClose<CR>'}
-map {'n', '<C-t>', ':tabnew<CR>:wincmd p<CR>'}
+map {'<Tab>', ':w!<CR>:BufferNext<CR>'}
+map {'<S-Tab>', ':w!<CR>:BufferPrevious<CR>'}
+map {'<C-w>', ':BufferClose<CR>'}
+map {'<C-t>', ':tabnew<CR>:wincmd p<CR>'}
 
 -- Fuzzy File Search
-map {'n', '<C-p>', ':Files<CR>'}
+map {'<C-p>', ':Files<CR>'}
 
 -- Competitive programming
-map {'n', '<C-r>', ':Test<CR>'}
+map {'<C-r>', ':Test<CR>'}
 
