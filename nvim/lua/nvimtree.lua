f4e1dac99b454e4eda1bce0eea8a5a492cbafdfa
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-02-13 23:41:03 +0530

    Changed neovim file tree to open with fixed width

--- nvim/lua/nvimtree.lua
+++ nvim/lua/nvimtree.lua
@@ -1,12 +1,14 @@
 -- NvimTree customization
 require'nvim-tree'.setup {
     auto_close = true,
+	open_on_setup = true,
     open_on_tab = true,
 	update_cwd = true,
 	view = {
 		auto_resize = true
-	},
-	filters = {
-		dotfiles = true
 	}
 }
+
+-- Autostart NvimTree with a directory is opened
+vim.cmd "autocmd StdinReadPre * let s:std_in=1"
+vim.cmd "autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') | tabnew | execute 'NvimTreeOpen' | execute 'lua require\"bufferline.state\".set_offset(31)' | execute 'cd '.argv()[0] | endif"
