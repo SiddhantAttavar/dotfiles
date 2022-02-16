7a38b825c5544614c705a80b5625206d66111e98
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-02-16 10:22:32 +0530

    Update neovim config

--- nvim/lua/nvimtree.lua
+++ nvim/lua/nvimtree.lua
@@ -1,9 +1,9 @@
 -- NvimTree customization
 require'nvim-tree'.setup {
 	hijack_netrw = false,
-    auto_close = true,
+	auto_close = true,
 	open_on_setup = false,
-    open_on_tab = false,
+	open_on_tab = true,
 	update_cwd = true,
 	view = {
 		auto_resize = true
@@ -13,4 +13,3 @@ require'nvim-tree'.setup {
 -- Set offset for barbar is it is started with a directory argument
 vim.cmd "autocmd StdinReadPre * let s:std_in=1"
 vim.cmd "autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') | execute 'NvimTreeOpen' | execute 'lua require\"bufferline.state\".set_offset(31)' | execute 'cd '.argv()[0] | endif"
---vim.cmd "autocmd VimEnter * if argc() == 0 | tabnew | endif"
