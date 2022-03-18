46ebe2555efee3f2f66285dcf6678373cd8a140b
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-03-18 11:17:18 +0530

    Add .dircolors to termux setup script

--- termux-setup.sh
+++ termux-setup.sh
@@ -51,6 +51,7 @@ mkdir -p $HOME/bin
 echo "Creating symlinks"
 createSymlink bash/.bashrc .bashrc
 createSymlink bash/.hushlogin .hushlogin
+createSymlink bash/.dircolors .dircolors
 createSymlink nvim .config/nvim
 createSymlink neofetch .config/neofetch
 
