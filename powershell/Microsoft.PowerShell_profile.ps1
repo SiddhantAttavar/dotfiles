390f0c7b474bfae76a50e3ee851108b5676d72ef
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-03-31 22:07:35 +0530

    Add powershell aliases

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,20 @@
 # ALIASES
-# some Get-ChildItem aliases
+# some ls aliases
 Set-Alias ll 'Get-ChildItem -Force'
 Set-Alias la 'Get-ChildItem -Force'
 Set-Alias l 'Get-ChildItem'
 
+# rm alias
+If (Test-Path Alias:rm) {Remove-Item Alias:rm}
+If (Test-Path Alias:rm) {Remove-Item Alias:rm}
+function rm {Remove-Item -Recurse $args}
+
 # touch alias
 Set-Alias touch 'New-Item'
 
+# git aliases
+function gsync {git pull; git push}
+function gca {git add .; git commit -m $args[0]}
+
+# winget aliases
+function update {winget upgrade --all --accept-package-agreements --accept-source-agreements}
