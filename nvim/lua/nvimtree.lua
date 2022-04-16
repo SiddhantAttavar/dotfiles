ec1fff9e784a072ffb8bfe56b817635da3b0890d
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-04-16 18:45:57 +0530

    nvim fix opening file settings

--- nvim/lua/nvimtree.lua
+++ nvim/lua/nvimtree.lua
@@ -2,7 +2,6 @@
 require'nvim-tree'.setup {
 	hijack_netrw = false,
 	disable_netrw = true,
-	auto_close = true,
 	open_on_setup = false,
 	open_on_tab = true,
 	update_cwd = true,
@@ -11,6 +10,9 @@ require'nvim-tree'.setup {
 	}
 }
 
+vim.opt.splitright = true
+
 -- Set offset for barbar is it is started with a directory argument
 vim.cmd "autocmd StdinReadPre * let s:std_in=1"
 vim.cmd "autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') | execute 'lua require\"bufferline.state\".set_offset(31)' | endif"
+vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
