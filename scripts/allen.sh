6ce246daea1d1d3067cf45f2a6459507de1a6e3a
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-05-01 21:26:47 +0530

    Add bash allen script

---
+++ scripts/allen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for pdf in $DOWNLOADS_DIR/*.pdf; do
+	read -p "${pdf##*/} Subject: " subject;
+
+	if [ $subject == 'p' ] ; then
+		mv "$pdf" "$ALLEN_DIR/Physics";
+	elif [ $subject == 'c' ] ; then
+		mv "$pdf" "$ALLEN_DIR/Chemistry";
+	elif [ $subject == 'm' ] ; then
+		mv "$pdf" "$ALLEN_DIR/Maths";
+	elif [ $subject == 'o' ] ; then
+		mv "$pdf" "$ALLEN_DIR/Others";
+	fi
+done
