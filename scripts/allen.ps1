cffa810b700c6efdd0dce03dfb9a56178afa22a1
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-05-01 20:05:37 +0530

    Update allen script

--- scripts/allen.ps1
+++ scripts/allen.ps1
@@ -2,22 +2,18 @@ $downloadsDir = 'D:/Downloads';
 $allenDir = 'D:/Allen/Grade 12/';
 
 foreach ($pdf in Get-ChildItem $downloadsDir -Filter '*.pdf') {
-	if((Get-Content $pdf.FullName | select -first 1 ) -like "%PDF-1.5*") {
-		$subject = Read-Host "$pdf.FullName Subject: ";
-		$destDir = $allenDir;
-		if ($subject == 'p') {
-			$destDir += 'Physics';
-		}
-		else if ($subject == 'c') {
-			$destDir += 'Chemistry';
-		}
-		else if ($subject == 'm') {
-			$destDir += 'Mathematics';
-		}
-		else if ($subject == 'o') {
-			$destDir += 'Others';
-		}
-
-		Move-Item $pdf $destDir;
+	$subject = Read-Host "$pdf Subject: ";
+	$destDir = $allenDir;
+	if ($subject -eq 'p') {
+		Move-Item $downloadsDir/$pdf "$destDir/Physics";
+	}
+	elseif ($subject -eq 'c') {
+		Move-Item $downloadsDir/$pdf "$destDir/Chemistry";
+	}
+	elseif ($subject -eq 'm') {
+		Move-Item $downloadsDir/$pdf "$destDir/Mathematics";
+	}
+	elseif ($subject -eq 'o') {
+		Move-Item $downloadsDir/$pdf "$destDir/Others";
 	}
 }
