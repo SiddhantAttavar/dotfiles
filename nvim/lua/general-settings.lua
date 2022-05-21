ff67ffb31393e283cd700b8fd25c6f8563fefb61
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-05-21 23:17:00 +0530

    Update neovim indentation settings

--- nvim/lua/general-settings.lua
+++ nvim/lua/general-settings.lua
@@ -6,9 +6,11 @@ vim.o.linebreak = true
 
 -- Indentation settings
 vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.softtabstop = 0 
 vim.o.expandtab = false
-vim.cmd "autocmd VimEnter * set autoindent | filetype indent on"
+vim.o.smartindent = true
+vim.o.autoindent = true
 
 -- Copy/paste from system clipboard
 vim.o.clipboard = 'unnamedplus'
