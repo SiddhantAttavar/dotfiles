3a5555fee2dd494ae4ac5ab1c0bdd81ee0c27a92
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-06-18 11:26:32 +0530

    nvim: Remove markdown lsp

--- nvim/lua/nvim-lspconfig.lua
+++ nvim/lua/nvim-lspconfig.lua
@@ -3,18 +3,18 @@ local lsp_configs = require('lspconfig.configs')
 local lsp_util = require('lspconfig.util')
 
 -- prosemd
-lsp_configs.prosemd = {
-  default_config = {
-    -- Update the path to prosemd-lsp
-    cmd = { "~/.local/bin/prosemd-lsp", "--stdio" },
-    filetypes = { "markdown" },
-    root_dir = function(fname)
-      return lsp_util.find_git_ancestor(fname) or vim.fn.getcwd()
-    end,
-    settings = {},
-  }
-}
-lsp.prosemd.setup{ on_attach = on_attach }
+--lsp_configs.prosemd = {
+--  default_config = {
+--    -- Update the path to prosemd-lsp
+--    cmd = { "~/.local/bin/prosemd-lsp", "--stdio" },
+--    filetypes = { "markdown" },
+--    root_dir = function(fname)
+--      return lsp_util.find_git_ancestor(fname) or vim.fn.getcwd()
+--    end,
+--    settings = {},
+--  }
+--}
+--lsp.prosemd.setup{ on_attach = on_attach }
 
 -- pyright
 lsp.pyright.setup{ on_attach = on_attach }
