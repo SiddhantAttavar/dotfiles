4ead899c7d420aacbfee2bd00610bdd6d0fe58b1
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-10-25 16:02:50 +0530

    qutebrowser: Change tab keybindings

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,8 +1,3 @@
 # Themes
 config.source('qutebrowser-themes/themes/onedark.py')
-
-# Keybindings
-config.bind('<Ctrl-Tab>', 'tab-next')
-config.bind('<Ctrl-Shift-Tab>', 'tab-next')
-
 config.load_autoconfig()
