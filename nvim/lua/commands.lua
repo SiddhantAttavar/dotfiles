4c82071277cdfaae985dd8a9ea92503e4d6b10ce
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-12-11 22:02:20 +0530

    nvim: Add gcap command

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,1 +1,6 @@
 -- Commands for neovim
+vim.api.nvim_create_user_command(
+	'Gcap',
+	'Git add . | Git commit -m <args> | Git push',
+	{nargs = 1, bang = true, desc = 'Stage commit and push all files in current working folder'}
+)
