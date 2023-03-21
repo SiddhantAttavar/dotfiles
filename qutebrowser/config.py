7ce46165f6d3b399b1035510bb6aeb77aefd56f2
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-03-21 10:29:46 +0530

    pass: Setup pass password manager

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,3 +1,4 @@
 # Themes
 config.source('qutebrowser-themes/themes/onedark.py')
 config.load_autoconfig()
+config.bind('<z>', 'spawn --userscript qute-pass --username-target secret --username-pattern "login: (.+)"')
