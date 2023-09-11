c10371eb5b6e1cf8bdcf7b44edb8297acb940547
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-09-11 13:25:52 +0530

    qutebrowser: update quickmarks

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,7 +1,7 @@
 -- Commands for neovim
 vim.api.nvim_create_user_command(
 	'Gcap',
-	'Git pull | Git add . | Git commit -m <args> | Git push',
+	'Git pull | Git add -A | Git add . | Git commit -m <args> | Git push',
 	{
 		nargs = 1,
 		bang = true,
@@ -11,7 +11,7 @@ vim.api.nvim_create_user_command(
 
 vim.api.nvim_create_user_command(
 	'Gca',
-	'Git add . | Git commit -m <args>',
+	'Git pull | Git add -A | Git add . | Git commit -m <args>',
 	{
 		nargs = 1,
 		bang = true,
