5a0c09f6a96662aa8358ca7b9df08a05ca85336f
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-01-03 10:02:14 +0530

    enale markdown syntaz highlighting

--- nvim/lua/ft-groups.lua
+++ nvim/lua/ft-groups.lua
@@ -2,6 +2,6 @@
 return {
 	lsp_fts = { 'python', 'cpp', 'markdown', 'lua' },
 	dap_fts = { 'python' },
-	treesitter_fts = { 'python', 'cpp', 'java', 'lua' },
+	treesitter_fts = { 'python', 'cpp', 'java', 'lua', 'markdown' },
 	text_fts = { 'markdown', 'txt' }
 }
