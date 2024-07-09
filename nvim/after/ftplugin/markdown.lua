289f3c6bea921ad1cb1dbd8b6aa3ab73250d25ca
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-07-09 12:18:40 +0530

    nvim: notebooks

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,4 +3,5 @@ if not vim.g.vscode then
 	vim.opt_local.tabstop = 4
 	vim.opt_local.expandtab = false
 	vim.cmd [[silent exec "GuessIndent"]]
+	require("quarto").activate()
 end
--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -9,10 +9,10 @@ return {
         keys = {
             { '<localleader>hi', ':MoltenInit<CR>' },
             { '<localleader>hr', ':MoltenReevaluateCell<CR>' },
-            { '<localleader>h', ':<C-u>MoltenEvaluateVisual<CR>gv', mode = 'v' },
+            { '<localleader>hv', ':<C-u>MoltenEvaluateVisual<CR>gv', mode = 'v' },
             { '<localleader>hh', ':MoltenHideOutput<CR>' },
             { '<localleader>hs', ':MoltenShowOutput<CR>' },
-            { '<localleader>hl', ':MoltenEvaluateLine<CR>' },
+            -- { '<localleader>hl', ':MoltenEvaluateLine<CR>' },
             { '<localleader>hd', ':MoltenDelete<CR>' },
             { '<localleader>hx', ':MoltenOpenInBrowser<CR>' },
         },
@@ -25,6 +25,7 @@ return {
         end
 	},
 
+    -- Jupytext
     {
         'GCBallesteros/jupytext.nvim',
         lazy = false,
@@ -35,4 +36,43 @@ return {
             force_ft = 'markdown'
         }
     },
+
+    -- Quarto
+    {
+        'quarto-dev/quarto-nvim',
+        dependencies = { 'jmbuhr/otter.nvim', 'nvim-treesitter/nvim-treesitter' },
+        keys = {
+            { "<localleader>hc", ':lua require("quarto.runner").run_cell()<CR>' },
+            { "<localleader>ha", ':lua require("quarto.runner").run_above()<CR>' },
+            { "<localleader>hA", ':lua require("quarto.runner").run_all()<CR>' },
+            { "<localleader>hl", ':lua require("quarto.runner").run_line()<CR>' },
+            { "<localleader>hr",  ':lua require("quarto.runner").run_range()<CR>', mode = 'v' },
+        },
+        ft = { 'quarto', 'markdown' },
+        config = true,
+        opts = {
+            lspFeatures = {
+                languages = { 'python' },
+                chunks = 'all',
+                diagnostics = {
+                    enabled = true,
+                    triggers = { 'BufWritePost' }
+                },
+                completion = {
+                    enabled = true
+                }
+            },
+            keymap = {
+                hover = 'H',
+                definition = 'gd',
+                rename = '<Leader>r',
+                references = 'gr',
+                format = '<Leader>gf'
+            },
+            codeRunner = {
+                enabled = true,
+                default_method = 'molten'
+            }
+        }
+    }
 }
